660b4957b6e23173df2719f3310f270a2c918770
Nic167 <nictan0073@gmail.com>
2025-05-11 02:05:01 +0800

    Added additional setting to force js files to be sent as application/javascript instead of text/plain

--- FullStack-Language/policylens/policylens/settings.py
+++ FullStack-Language/policylens/policylens/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import mimetypes
+
+# Force js files to be sent as application/javascript instead of text/plain
+mimetypes.add_type("application/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
